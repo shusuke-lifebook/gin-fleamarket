2c626c5c68fc0930505a7eeb7253c6502bdadd1b
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-07-18 15:59:30 +0900

    データの取得処理、更新処理、削除処理を実装

--- main.go
+++ main.go
@@ -23,11 +23,11 @@ func main() {
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
-	router := gin.Default()
-	router.GET("/items", itemController.FindAll)
-	router.GET("/items/:id", itemController.FindById)
-	router.POST("/items", itemController.Create)
-	router.PUT("/items/:id", itemController.Update)
-	router.DELETE("/items/:id", itemController.Delete)
-	router.Run("localhost:8080")
+	r := gin.Default()
+	r.GET("/items", itemController.FindAll)
+	r.GET("/items/:id", itemController.FindById)
+	r.POST("/items", itemController.Create)
+	r.PUT("/items/:id", itemController.Update)
+	r.DELETE("/items/:id", itemController.Delete)
+	r.Run("localhost:8080")
 }
