d9791752eca58e3e43958b24ee8c80846376c22a
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-07-18 15:27:09 +0900

    データ登録処理の実装

--- repositories/item_repository.go
+++ repositories/item_repository.go
@@ -4,6 +4,8 @@ package repositories
 import (
 	"errors"
 	"gin-fleamarket/models"
+
+	"gorm.io/gorm"
 )
 
 type IItemRepository interface {
@@ -59,4 +61,41 @@ func (r *ItemMemoryRepository) Delete(itemId uint) error {
 		}
 	}
 	return errors.New("Item not found")
+}
+
+type ItemRepository struct {
+	db *gorm.DB
+}
+
+// Create implements [IItemRepository].
+func (r *ItemRepository) Create(newItem models.Item) (*models.Item, error) {
+	result := r.db.Create(&newItem)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &newItem, nil
+}
+
+// Delete implements [IItemRepository].
+func (i *ItemRepository) Delete(itemId uint) error {
+	panic("unimplemented")
+}
+
+// FindAll implements [IItemRepository].
+func (i *ItemRepository) FindAll() (*[]models.Item, error) {
+	panic("unimplemented")
+}
+
+// FindById implements [IItemRepository].
+func (i *ItemRepository) FindById(itemId uint) (*models.Item, error) {
+	panic("unimplemented")
+}
+
+// Update implements [IItemRepository].
+func (i *ItemRepository) Update(updateItem models.Item) (*models.Item, error) {
+	panic("unimplemented")
+}
+
+func NewItemRepository(db *gorm.DB) IItemRepository {
+	return &ItemRepository{db: db}
 }
