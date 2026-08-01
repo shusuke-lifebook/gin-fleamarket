bd2d8fce6a8ee9e5b5ee6e341e136378f5598989
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-08-01 20:42:15 +0900

    基本的なAPIのテスト

--- main_test.go
+++ main_test.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"gin-fleamarket/infra"
 	"gin-fleamarket/models"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 )
 
@@ -53,4 +57,24 @@ func setup() *gin.Engine {
 	router := setupRouter(db)
 
 	return router
+}
+
+func TestFindAll(t *testing.T) {
+	// テストのセットアップ
+	router := setup()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/items", nil)
+
+	// APIリクエストに実行
+	router.ServeHTTP(w, req)
+
+	// APIの実行結果の取得
+	var res map[string][]models.Item
+	json.Unmarshal([]byte(w.Body.String()), &res)
+
+	// アサーション
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, 3, len(res["data"]))
+
 }
